a1a7f19301cd7cdc429e32869b29ef60ea730062
GarckushaVladimir <vladimgarckusha@yandex.ru>
2025-02-17 22:50:16 +0300

    added select methods with filters

--- select_methods_dao.py
+++ select_methods_dao.py
@@ -20,7 +20,29 @@ async def select_username_id(session):
     return await UserDAO.get_username_id(session)
 
 
-rez = run(select_username_id())
-for i in rez:
-    rez = UsernameIdPydantic.from_orm(i)
-    print(rez.dict())
+# rez = run(select_username_id())
+# for i in rez:
+#     rez = UsernameIdPydantic.from_orm(i)
+#     print(rez.dict())
+
+@connection
+async def select_full_user_info(session, user_id: int):
+    rez = await UserDAO.find_one_or_none_by_id(session=session, data_id=user_id)
+    if rez:
+        return UserPydantic.from_orm(rez).dict()
+    return {"message": f'Пользователь с ID {user_id} не найден!'}
+
+
+# info = run(select_full_user_info(user_id=8))
+# print(info)
+
+@connection
+async def select_full_user_info_email(session, user_id: int, email: str):
+    rez = await UserDAO.find_one_or_none(session=session, id=user_id, email=email)
+    if rez:
+        return UserPydantic.from_orm(rez).dict()
+    return {"message": f'Пользователь с ID {user_id} не найден!'}
+
+
+# info = run(select_full_user_info_email(user_id=9, email='john.doe12@example.com'))
+# print(info)
